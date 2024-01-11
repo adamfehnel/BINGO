0d301095ad182f737c1e0e7452d0fe8cbbb7f904
Adam Fehnel <adam_fehnel@putnam.com>
2024-01-11 16:10:40 -0500

    Defining some structure

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgbCollapseModule,
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+	isMenuCollapsed = true;
 
 }
